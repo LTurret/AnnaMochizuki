9c1a5605cbd50f17e3da785d6cd0d993db8565e9
Geroge Rupp <a0919329@gmail.com>
2020-05-19 21:11:30 +0800

    Update main.py
    笑死 來亂玩的

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ async def on_member_remove(member):
     channel = Misaki.get_channel(474858135853596675)
     await channel.send(f'{member.mention} 退出了漢堡群，SAD！')
 
+@Misaki.listen()
+async def on_message(message):
+     if "help" is message.content:
+        channel = Misaki.get_channel(474858135853596675)
+        await channel.send('okay here\'s list...')
+     elif "鄭順謙" is message.content:
+        channel = Misaki.get_channel(474858135853596675)
+        await channel.send('還錢')
+     elif "水塔" is message.content:
+        channel = Misaki.get_channel(474858135853596675)
+        await channel.send('鄭順謙')
+
 @Misaki.command()
 async def ping(ctx):
     if (round(Misaki.latency*1000) >= 100):
@@ -24,15 +36,5 @@ async def ping(ctx):
         await ctx.send('有夠遠的 :ok_this_is_epic:')
     else:
         await ctx.send(f'你與我的距離為 {round(Misaki.latency*1000)} 毫秒')
-
-@Misaki.command()
-async def 鄭順謙(ctx):
-    await ctx.send('還錢')
-
-@Misaki.command()
-async def 水塔(ctx):
-    await ctx.send('鄭順謙')
-    await ctx.send('還錢')
-
 
 Misaki.run('NzEyMjQzMDQwNzYwMTAyOTky.XsOu7A.6UD4xvWSbWH0LSIy_A8mgFNOEmA')
