c55aa9aa6efde0363630e6e95c620f82c1cd1588
Geroge Rupp <a0919329@gmail.com>
2020-05-19 21:34:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Misaki = commands.Bot(command_prefix='|')
 
 @Misaki.event
 async def on_ready():
-    print("Misaki is working!")
+    print("Misaki is working!\n大家的事務員，美咲上班啦！")
 
 @Misaki.event
 async def on_member_join(member):
@@ -26,6 +26,9 @@ async def on_message(message):
      if "help" == message.content:
         channel = Misaki.get_channel(int(jdata['MainChannelID']))
         await channel.send('okay here\'s list...')
+     elif "水塔" == message.content:
+        channel = Misaki.get_channel(int(jdata['MainChannelID']))
+        await channel.send('鄭順謙')
 
 @Misaki.command()
 async def ping(ctx):
