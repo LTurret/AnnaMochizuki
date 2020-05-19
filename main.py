7ca5db8ab172c2d7b7ebcfe7b7478f087c886142
Geroge Rupp <a0919329@gmail.com>
2020-05-19 19:06:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ Misaki = commands.Bot(command_prefix='|')
 async def on_ready():
     print("Misaki is working!")
 
+@Misaki.event
+async def on_member_join(member):
+    print(f'{member} 加入了漢堡群！')
+
+@Misaki.event
+async def on_member_leave(member):
+    print(f'{member} 退出了漢堡群，SAD')
+
 Misaki.run('NzEyMjQzMDQwNzYwMTAyOTky.XsOu7A.6UD4xvWSbWH0LSIy_A8mgFNOEmA')
