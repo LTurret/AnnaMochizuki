cfc6c075eb8811612dfaa5892df97df58d986c6b
Geroge Rupp <a0919329@gmail.com>
2020-05-20 17:13:26 +0800

    Update Misaki.py
    - cog added
    - setting.json is not correct location while coding with other computer

--- Misaki.py
+++ Misaki.py
@@ -21,38 +21,12 @@ async def on_member_remove(member):
     channel = Misaki.get_channel(int(jdata['MainChannelID']))
     await channel.send(f'{member.mention} 退出了漢堡群，SAD！')
 
-@Misaki.listen()
-async def on_message(message):
-     if "help" == message.content:
-        channel = Misaki.get_channel(int(jdata['MainChannelID']))
-        await channel.send('okay here\'s list...')
-     elif "水塔" == message.content:
-        channel = Misaki.get_channel(int(jdata['MainChannelID']))
-        await channel.send('鄭順謙')
+@Misaki.command()
+async def load(ctx, extension):
+    Misaki.load_extension(f'cogs.[extension]')
 
 @Misaki.command()
-async def ping(ctx):
-    if (round(Misaki.latency*1000) >= 100):
-        await ctx.send(f'你與我的距離為 {round(Misaki.latency*1000)} 毫秒\n有夠遠的')
-    else:
-        await ctx.send(f'你與我的距離為 {round(Misaki.latency*1000)} 毫秒')
-
-@Misaki.command()
-async def purge(ctx, amount=1):
-	await ctx.channel.purge(limit=amount+1)
-
-@Misaki.command()
-async def 大和美少女(ctx):
-    pic = discord.File(jdata['大和美少女'])
-    await ctx.send(file=pic)
-
-@Misaki.command()
-async def 南斗(ctx):
-    pic = discord.File(jdata['南斗'])
-    await ctx.send(file=pic)
-
-@Misaki.command()
-async def 歌單(ctx):
-    await ctx.send(jdata['歌單'])
+async def unload(ctx, extension):
+    Misaki.unload_extension(f'cogs.[extension]')
 
 Misaki.run(jdata['BotToken'])
