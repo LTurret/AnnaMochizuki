90141e5784afc439a19ec8cfd5e93d2885de8770
Geroge Rupp <a0919329@gmail.com>
2020-05-20 17:14:21 +0800

    Create MainCommands.py
    the main commands for this bot

---
+++ commands/MainCommands.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class MainCommands(commands.Cog):
+    def __init__(self, Misaki):
+        self.bot = Misaki
+     
+    @commands.command()
+    async def ping(self, ctx):
+        if (round(self.bot.latency*1000) >= 100):
+            await ctx.send(f'你與我的距離為 {round(self.bot.latency*1000)} 毫秒\n有夠遠的')
+        else:
+            await ctx.send(f'你與我的距離為 {round(self.bot.latency*1000)} 毫秒')
+
+    @commands.command()
+    async def purge(self, ctx, amount=1):
+        await ctx.channel.purge(limit=amount+1)
+
+def setup(Misaki):
+    Misaki.add_cog(MainCommands(Misaki))
