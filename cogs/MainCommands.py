8e611853eef214fef3cfcba6b25c56199204d997
Geroge Rupp <a0919329@gmail.com>
2020-05-20 22:00:17 +0800

    Create MainCommands.py
    rewrite cogs

---
+++ cogs/MainCommands.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class MainCommands(commands.Cog):
+
+    def __init__(self, Misaki):
+        self.Misaki = Misaki
+
+    @commands.command()
+    async def ping(self, ctx):
+        if (round(self.Misaki.latency*1000) >= 100):
+            await ctx.send(f'你與我的距離為 {round(self.Misaki.latency*1000)} 毫秒\n有夠遠的')
+        else:
+            await ctx.send(f'你與我的距離為 {round(self.Misaki.latency*1000)} 毫秒')
+
+    @commands.command()
+    async def purge(self, ctx, amount=1):
+        await ctx.channel.purge(limit=amount+1)
+
+def setup(Misaki):
+    Misaki.add_cog(MainCommands(Misaki))
