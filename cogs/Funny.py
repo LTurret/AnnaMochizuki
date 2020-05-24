ffad5319a3b08100b9b00738666f024b0b6b955d
Geroge Rupp <a0919329@gmail.com>
2020-05-25 00:15:16 +0800

    Update Funny.py

--- cogs/Funny.py
+++ cogs/Funny.py
@@ -2,26 +2,12 @@ import discord
 import json
 from discord.ext import commands
 
-with open ('setting.json', 'r', encoding='utf8') as jsonSetting:
+with open (r"C:\Users\George Rupp\Desktop\Files\Programming\Github\Suspend-bot\json\setting.json", 'r', encoding='utf8') as jsonSetting:
     jdata = json.load(jsonSetting)
 
 class Funny(commands.Cog):
     def __init__(self, Misaki):
         self.Misaki = Misaki
-
-    @commands.command()
-    async def 大和美少女(self, ctx):
-        pic = discord.File(jdata['大和美少女'])
-        await ctx.send(file=pic)
-
-    @commands.command()
-    async def 南斗(self, ctx):
-        pic = discord.File(jdata['南斗'])
-        await ctx.send(file=pic)
-
-    @commands.command()
-    async def 歌單(self, ctx):
-        await ctx.send(jdata['歌單'])
 
     @commands.Cog.listener()
     async def on_message(self, message):
