89622ac02529aa2956b37aae5ca61801541f8cd5
Geroge Rupp <a0919329@gmail.com>
2020-06-08 22:41:07 +0800

    Create Other.py

---
+++ cogs/Other.py
@@ -0,0 +1,24 @@
+import discord
+import json
+import datetime
+from discord.ext import commands
+
+class Other(commands.Cog):
+    def __init__(self, Misaki):
+        self.Misaki = Misaki
+
+    @commands.command()
+    async def 乘法公式(self, ctx):
+        embed = discord.Embed(title = "乘法公式", description = None, colour = 0x93e2df
+        , timestamp = datetime.datetime.utcnow())
+        embed.add_field(name = "a²-b²", value = "(a+b)(a-b)", inline = False)
+        embed.add_field(name = "a³±b³", value = "(a±b)(a²±ab+b²)", inline = False)
+        embed.add_field(name = "(a±b)²", value = "a²±2ab+b²", inline = False)
+        embed.add_field(name = "(a+b)³", value = "a³-3a²b+3ab²+b³ \n= a³+b³+3ab(a+b)", inline = False)
+        embed.add_field(name = "(a-b)³", value = "a³-3a²b+3ab²-b³ \n= a³-b³-3ab(a-b)", inline = False)
+        await ctx.send(embed=embed)
+
+        #add some fun contest commands
+
+def setup(Misaki):
+    Misaki.add_cog(Other(Misaki))
