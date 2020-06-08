4f286fd5b6c02ac7e71dcce76bce7bd2a32a8a75
Geroge Rupp <a0919329@gmail.com>
2020-06-08 22:41:05 +0800

    Update MainCommands.py

--- cogs/MainCommands.py
+++ cogs/MainCommands.py
@@ -3,7 +3,6 @@ import json
 import random
 import datetime
 from discord.ext import commands
-from typing import Optional
 
 with open (r"C:\Users\George Rupp\Desktop\Files\Programming\Github\Suspend-bot\json\MLTD.json", 'r', encoding='utf8') as MLTDjson:
     Mjson = json.load(MLTDjson)
@@ -16,13 +15,10 @@ class MainCommands(commands.Cog):
     async def help_main(self, ctx):
         embed = discord.Embed(title = "主要指令清單", description = None, colour = 0x93e2df
         , timestamp = datetime.datetime.utcnow())
-        #embed.set_author(name = "青羽美咲", url = Mjson['MLTD_Misaki_about'], icon_url = Mjson['MLTD_765_icon'])
-        embed.set_thumbnail(url = "https://i.imgur.com/eEKg1Vn.jpg")
         embed.add_field(name = "> 與我的延遲", value = "**@青羽美咲 ping**", inline = False)
         embed.add_field(name = "> 刪除數量訊息", value = "**@青羽美咲 purge [數量]**", inline = False)
         embed.add_field(name = "> 匿名訊息", value = "**@青羽美咲 botsaid [訊息]**", inline = False)
         embed.add_field(name = "> 隨機組隊", value = "**@青羽美咲 rds [關鍵字] [每組幾人] [隊伍數]**", inline = False)
-        embed.set_footer(text = "なんとぉー！")
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -57,10 +53,8 @@ class MainCommands(commands.Cog):
                 hasNick.remove(removal)
             embed = discord.Embed(title = f'第 {iteration+1} 小隊', description = None, colour = 0x93e2df
             , timestamp = datetime.datetime.utcnow())
-            #embed.set_author(name = "青羽美咲", url = Mjson['MLTD_Misaki_about'], icon_url = Mjson['MLTD_765_icon'])
-            embed.set_thumbnail(url='https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/flag-icon.png')
-            embed.add_field(name="隊員", value=f'{enum}', inline=False)
-            embed.set_footer(text = "なんとぉー！")
+            embed.set_thumbnail(url = 'https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/flag-icon.png') #TO JSON TO JSON TO JSON
+            embed.add_field(name = "隊員", value=f'{enum}', inline=False)
             await ctx.send(embed=embed)
 
 def setup(Misaki):
