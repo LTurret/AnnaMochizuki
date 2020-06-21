e01547201187b3609ddc0e65b0c7213403ed66e1
Geroge Rupp <a0919329@gmail.com>
2020-06-21 10:57:25 +0800

    Update MLTD.py

--- cogs/MLTD.py
+++ cogs/MLTD.py
@@ -46,9 +46,9 @@ class MLTD(commands.Cog):
     async def 轉蛋機率(self, ctx):
         embed = discord.Embed(title = "轉蛋機率"
         , description = None, colour = 0x93e2df, timestamp = datetime.datetime.utcnow())
-        embed.add_field(name = "> <:SSR:723921126560104539>", value = '**"提供機率" 3%，也就是 "轉出機率" <= "3%"**', inline = False)
-        embed.add_field(name = "> <:SR:723921117013606460>", value = '**"提供機率" 12%，也就是 "轉出機率" <= "15"**', inline = False)
-        embed.add_field(name = "> <:R_:723921106909659227>", value = '**"提供機率" 85%，也就是 "轉出機率" > "15"**', inline = False)
+        embed.add_field(name = "> <:SSR:723921126560104539>", value = '**"提供機率" 3%，也就是 "轉出機率" <= 3%**', inline = False)
+        embed.add_field(name = "> <:SR:723921117013606460>", value = '**"提供機率" 12%，也就是 "轉出機率" <= 15%**', inline = False)
+        embed.add_field(name = "> <:R_:723921106909659227>", value = '**"提供機率" 85%，也就是 "轉出機率" > 15%**', inline = False)
         await ctx.send(embed=embed)
 
     @commands.command()
