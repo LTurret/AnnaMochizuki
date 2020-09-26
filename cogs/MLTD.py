3c3d72b07670ad398f459a80a1a6ab5462a6a560
Geroge Rupp <a0919329@gmail.com>
2020-09-26 12:23:44 +0800

    Update MLTD.py

--- cogs/MLTD.py
+++ cogs/MLTD.py
@@ -2,6 +2,8 @@ import discord
 import json
 import datetime
 import random
+import bs4
+import urllib.request as req
 from discord.ext import commands
 
 with open (r"C:\Users\a0919\Desktop\Files\Programming\Github\Suspend-bot\json\mltd.json", 'r', encoding='utf8') as MLTDjson:
@@ -74,6 +76,23 @@ class MLTD(commands.Cog):
                 embed.set_author(name = "早坂そら", url = Mjson['早坂空_about'], icon_url = Mjson['早坂空_avatar'])
                 embed.add_field(name = "> 抽獎結果，抽了10次有SR保底", value = f'{result}', inline = False)
                 await ctx.send(embed=embed)
+
+    @commands.command()
+    async def getData(self, character_name:str):
+        #put a character name list here
+        #serction 1.
+        #   put every character name to embed message to show off all option
+        url = "https://imas.gamedbs.jp/mlth/chara/show/24"
+        request = req.Request(url, headers= {
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+        })
+        with req.urlopen(request) as response:
+            data = response.read().decode("utf-8")
+        root = bs4.BeautifulSoup(data, "html.parser")
+        source = root.find_all("li", class_="hvr-grow")
+        for source_information in source:
+            character_name = source_information.a.get("title")[0:len(source_information)-6]
+            print(character_name)
 
 def setup(Misaki):
     Misaki.add_cog(MLTD(Misaki))
