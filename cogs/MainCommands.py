bf58df8d2e054bc162e1043834085957db1b7adb
Geroge Rupp <a0919329@gmail.com>
2021-07-23 02:29:23 +0800

    update

--- cogs/MainCommands.py
+++ cogs/MainCommands.py
@@ -2,11 +2,10 @@ import discord
 import json
 import datetime
 import random
-from discord.ext import commands
-from discord.ext.commands import command, Cog #button
+from discord.ext import commands, tasks
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
-from discord_components import DiscordComponents, Button, ButtonStyle, InteractionType
+from time import localtime, strftime
 
 with open (r"C:\Users\a0919\Desktop\Files\Programming\Github\Suspend-bot\json\MainCommands.json", 'r', encoding="utf8") as MainCommandsJson:
     MainCommandsJson = json.load(MainCommandsJson)
@@ -26,28 +25,9 @@ class MainCommands(commands.Cog):
         self.OuenResponseHolder = False
 
     @commands.command()
-    async def reply(self, ctx):
-        print(ctx)
-
-    @commands.command()
-    @commands.has_permissions(manage_messages = True)
-    async def purge(self, ctx, amount:int):
-        await ctx.channel.purge(limit = amount+1)
-        await ctx.channel.send(f'{amount} 個訊息已被刪除')
-
-    @commands.command()
-    @commands.has_permissions(manage_messages = True)
-    async def cls(self, ctx, amount:int):
-        await ctx.channel.purge(limit = amount+1)
-
-    @commands.command()
     async def botsaid(self, ctx, *,message):
         await ctx.message.delete()
         await ctx.send(message)
-
-    @commands.command()
-    async def rgs(self, ctx):
-        await ctx.channel.send("RGS is up!")
 
     @commands.command()
     async def rds(self, ctx, keyword:str, population:int, groups:int):
@@ -72,51 +52,26 @@ class MainCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        #引以為戒
-        if (message.content == "星辰" and message.author != self.Misaki.user):
-            await message.channel.send("<@!278453052850176000> 你必須引以為戒")
-
-
-        #明年一定第二
-        if (message.content == "明年一定二位" and message.author != self.Misaki.user):
-            await message.channel.send("<@!278453052850176000> 不可以食言耶")
-
-
-        #general - Twitter Mode
-        if (message.content.upper().count("TWITTER MODE") and len(message.content) != 12 and message.author != self.Misaki.user):
-            await message.add_reaction("❤️")
+        # Twitter Mode
+        if (message.content.upper().count("TWITTER MODE") and len(message.content) != 12 and message.author != self.Misaki):
             await message.add_reaction("🗨️")
             await message.add_reaction("🔁")
+            await message.add_reaction("❤️")
 
 
-        #iM@S
+        # iM@S
         TriggerPassword = message.content.count('TriggerWebhookConverter')
         if (TriggerPassword == True and message.author.display_name == "音無小鳥"):
             await message.delete()
             await message.channel.send(message.content[24:])
 
 
-        #Random Group-up System (RGS)
-        TriggerRGS = message.content.count("RGS is up!")
-        if (TriggerRGS == True and message.author == self.Misaki.user):
-            await message.channel.send("Random Group-up System voting is here!")
-            await message.add_reaction("🚩")
-
-
-        #RAS Detector
-        RaidDetector = message.content.count("Raid is start soon...")
-        if (RaidDetector == True and message.author == self.Misaki.user and self.BotSaidFilter == False):
-            self.RaidMessage = message
-            await message.edit(content = f"A Raid (:flag_tw:) afk will be starting in 10 seconds by <@{self.RaidAuthorId}>. Prepare to join raiding `{self.RaidAuthorName}'s {self.RaidChamberName}` *Now located above lounge.* **You do not need to react to anything**")
-            self.BotSaidFilter = True
-            self.RaidEndedSystemCaller = "Stady"
-            await message.add_reaction("🔚")
-
-        
-        #応援發送器
-        if (message.content == "(＊>△<)＜応援ください！" and message.author == self.Misaki.user):
+        # 応援 - 訊息發送器
+        if (message.content == "(＊>△<)＜応援ください！" and message.author == self.Misaki):
             self.OuenResponseHolder = True
-        #応援回應
+
+
+        # 応援 - 回應表情符號
         if (message.content.count("応援するよ") and self.OuenResponseHolder == True):
             self.OuenResponseHolder = False
             random_emojis = [
@@ -131,10 +86,9 @@ class MainCommands(commands.Cog):
             emoji = random_emojis[random.randint(0, 6)]
             await message.channel.send(emoji)
             
-
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        #ReactionRole
+        # Member verification system
         if (payload.channel_id == 463321768212299778 and payload.message_id == 464825427844792320 and str(payload.emoji) == "<:Serika:677696191772753940>"):
             guild = self.Misaki.get_guild(payload.guild_id)
             role = guild.get_role(711454063962882051)
@@ -142,34 +96,16 @@ class MainCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        #ReactionRole
+        # Member verification system
         if (payload.channel_id == 463321768212299778 and payload.message_id == 464825427844792320 and str(payload.emoji) == "<:Serika:677696191772753940>"):
             guild = self.Misaki.get_guild(payload.guild_id)
             member = guild.get_member(payload.user_id)
             role = guild.get_role(711454063962882051)
             await member.remove_roles(role)
 
-    @commands.command()
-    async def button(self, ctx):
-        await ctx.send(
-            content = "按鈕功能還在開發，目前沒有功能",
-            components = [
-                Button(
-                    label = "按這個會交互失敗",
-                    style = 4
-                ),
-                Button(
-                    label = "相關API",
-                    style = 5,
-                    url = "https://devkiki7000.gitbook.io/discord-components/"
-                )
-            ]
-        )
-
     @commands.Cog.listener()
     async def on_command_error(self, ctx, exception):
-        await ctx.channel.send(exception)      
+        await ctx.channel.send(exception)
 
 def setup(Misaki):
-    DiscordComponents(Misaki) # button
     Misaki.add_cog(MainCommands(Misaki))
